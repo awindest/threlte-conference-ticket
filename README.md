3ea94b9d66d08d092a2ab70ecd4cebb9359cc7fc
awindest <william.indest@gmail.com>
2024-08-10 09:00:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Threlte 6 at the Svelte Summit 2023
+
+This Repository contains the source code of the Svelte Summit 2023 talk "Threlte 6".
+
+## Installation
+
+```bash
+pnpm install
+```
+
+## Usage
+
+```bash
+pnpm dev
+```
+
+### Available Routes
+
+- `/` - The example that has been shown in the talk.
+- `/intro` - The example that has been shown in the intro of the talk, which is a slightly more polished version.
+
+### Blender Model
+
+The Blender model is located in `src/Blender/Ticket.blend`
---
+++ src/routes/Scene.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { T, useFrame } from '@threlte/core'
+	import {
+		Environment,
+		Text,
+		interactivity
+	} from '@threlte/extras'
+	import Ticket from './Ticket.svelte'
+	import {
+		DEG2RAD,
+		RAD2DEG
+	} from 'three/src/math/MathUtils'
+	import { spring } from 'svelte/motion'
+
+	let rotationY = 0
+
+	useFrame((_, delta) => {
+		rotationY += 0.1 * delta
+		if (rotationY * RAD2DEG > 90) {
+			rotationY -= 180 * DEG2RAD
+		}
+	})
+
+	interactivity()
+
+	const scale = spring(1)
+</script>
+
+<T.PerspectiveCamera
+	makeDefault
+	fov={10}
+	position={[0, 0, 40]}
+/>
+
+<Environment files="/oil-on-water.png" />
+
+<T.Group
+	rotation.y={rotationY}
+	on:pointerenter={() => {
+		scale.set(1.1)
+	}}
+	on:pointerleave={() => {
+		scale.set(1)
+	}}
+	scale={$scale}
+>
+	<Text
+		text="Bill Indest"
+		font="/Flama.otf"
+		fontSize={0.25}
+		position={[-0.8, -0.55, 0.04]}
+		maxWidth={1.5}
+	>
+		<T.MeshStandardMaterial envMapIntensity={11} />
+	</Text>
+
+	<Text
+		text="NO. 12345"
+		font="/Flama.otf"
+		fontSize={0.25}
+		position={[0, 1.88, 0.04]}
+		anchorX="50%"
+		anchorY="50%"
+	>
+		<T.MeshStandardMaterial
+			envMapIntensity={11}
+			color="black"
+			roughness={0}
+		/>
+	</Text>
+
+	<Ticket />
+</T.Group>
